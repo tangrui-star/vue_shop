e88c2ef0164cc017113af4905df2f1e35dd57ec9
tangrui-star <201803519@yangtzeu.edu.cn>
2020-11-15 19:40:15 +0800

    完成了权限功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
-import Welcome from "../components/Welcome.vue";
+import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
-
+import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -16,7 +17,9 @@ const routes = [
     redirect: '/Welcome',
     children: [
       {path: '/Welcome', component: Welcome},
-      {path: '/users', component: Users}
+      {path: '/users', component: Users},
+      {path: '/rights', component: Rights},
+      {path: '/roles', component: Roles}
       ]
   }
 ]
