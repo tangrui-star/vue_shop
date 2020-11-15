0d7891497fec4b0eeb56e14a67a0696da900884b
tangrui-star <201803519@yangtzeu.edu.cn>
2020-11-15 14:48:35 +0800

    完成用户列表功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,29 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input ,
+  Container ,
+  Header ,
+  Aside ,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox
+} from 'element-ui'
 // 导入弹框提示组件
 import { Message } from 'element-ui'
 
@@ -7,4 +31,24 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(TableColumn)
+Vue.use(Table)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
