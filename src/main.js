659c3bbc51548281c5676f8757f3d6c38d9ac2e2
tangrui-star <201803519@yangtzeu.edu.cn>
2020-12-19 09:55:51 +0800

    test

--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require style 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
@@ -24,8 +31,26 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
+// 在全局定义了时间过滤器
+Vue.filter('dateFormat',function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
 // 注册为全局变量
 Vue.component('tree-table',TreeTable)
+
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
 new Vue({
   router,
   render: h => h(App)
